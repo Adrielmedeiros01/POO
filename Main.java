dccc3cac2c4b9217d7db435c4c7fe59e1b31864d
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2025-08-13 00:46:56 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package meuprojeto;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		Pessoas p1 = new Pessoas("Adriel", 23);
+		
+		p1.getNome();
+		p1.setNome("NomeSetado");
+		p1.andar();
+		p1.parar();
+
+		
+		
+		
+	}
+}
---
+++ Pessoas.java
@@ -0,0 +1,49 @@
+package meuprojeto;
+
+public class Pessoas {
+
+//	Atributos:
+	public String nome;
+	public int idade;
+	public boolean andando = false;
+	
+//	Construtor:
+	public Pessoas (String nome, int idade) {
+		
+		this.nome = nome;		
+		this.idade = idade;
+	}
+	
+//	Métodos:
+	void andar() {	
+		System.out.println(this.nome+" está andando...");
+		this.andando = true;
+	}
+	
+	void parar() {
+		System.out.println(this.nome +" parou...");
+		this.andando = false;
+	}
+
+	
+//	Métodos especiais GET E SET:
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public int getIdade() {
+		
+		return idade;
+	}
+	
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+}
