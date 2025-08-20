98f0851147f7b239fb43bb3cbbe97612cd12c1df
Adriel Medeiros <130292427+Athelstan0420@users.noreply.github.com>
2025-08-19 21:58:41 -0300

    Update Main.java

--- Revisão_JAVA/Main.java
+++ Revisão_JAVA/Main.java
@@ -16,3 +16,28 @@ public class Main {
 		
 	}
 }
+
+
+
+// package meuprojeto;
+
+// public class Main {
+	
+// 	public static void main (String[] args) {
+		
+	
+// 		Pessoas p1 = new Pessoas();
+		
+
+// 		p1.setnome("Adriel");
+// 		p1.setendereco("Centro");
+// 		p1.setcpf("000000000");
+// 		p1.apresentação();
+		
+		
+
+		
+		
+		
+// 	}
+// }
