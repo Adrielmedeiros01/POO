b9856a49c9e532b133aece220006014544c87c99
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-09-02 20:53:17 -0300

    Create Class.java

---
+++ Static/Class.java
@@ -0,0 +1,17 @@
+package aulaPOO;
+
+public class Alunos {
+	
+	public static void maiorMenor(int n1, int n2) {
+		
+		if (n1 > n2) {
+			System.out.println(n1+" é maior que "+n2);
+			
+		}
+		else {
+			System.out.println(n2+" é maior que "+n1);
+		}
+		
+	}
+
+}
