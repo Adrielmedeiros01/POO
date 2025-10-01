25d9f32f296c8e41b1570083ecd30cf5b8faf693
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-09-30 21:20:16 -0300

    Create Main.java

---
+++ HerancaPOO/Main.java
@@ -0,0 +1,15 @@
+package poo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Guerreiro g1 = new Guerreiro("Adriel", 100, 100);
+		g1.exibirStatus();
+		Mago m1 = new Mago("Marcos", 100, 100);
+		m1.exibirStatus();
+		
+	}
+	
+
+}
