6c5f8d6cd3077fc3b20e443dcf7d13a0e663c361
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-04 13:10:47 -0300

    Create Main.java

---
+++ SistemaTransporte/Main.java
@@ -0,0 +1,23 @@
+package Principal;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+		
+		Moto m1 = new Moto();
+		m1.exibirInfo();
+		
+		System.out.println("");
+		
+		Caminhao c1 = new Caminhao();
+		c1.exibirInfo();
+		
+		System.out.println("");
+		
+		Carro cr1 = new Carro();
+		cr1.exibirInfo();
+		
+	}
+	
+}
