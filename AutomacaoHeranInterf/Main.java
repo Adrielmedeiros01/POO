171c72dfc892d8600a5624f1559b1400fc35f6a9
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-04 15:25:41 -0300

    Create Main.java

---
+++ AutomacaoHeranInterf/Main.java
@@ -0,0 +1,18 @@
+package Principal;
+
+public class Main {
+	
+	public static void main (String[] args) {
+		
+	
+		
+		DispIntelig d1 = new DispIntelig();
+		
+		d1.on();
+		d1.status();
+		d1.off();
+		d1.status();
+	
+	}
+	
+}
