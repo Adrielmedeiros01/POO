daac0d9b49c6569f654c58cd9aa1f9c1ef8b61a1
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-11 17:51:09 -0300

    Create PagamentoCartao.java

---
+++ Excecoes/Pagamento/PagamentoCartao.java
@@ -0,0 +1,12 @@
+package Simulado;
+
+public class PagamentoCartao implements Pagamento{
+
+	@Override
+	public void processarPagamento(double valor) {
+		
+		System.out.println("Pagamento de R$"+ valor +" realizado via Cartão de Crédito.");
+		
+	}
+
+}
