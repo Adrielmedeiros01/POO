0e4f5978a18ac627f756422b6596d05467df6217
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-11 19:57:54 -0300

    Create SmartTv.java

---
+++ Prova2UND-1/Q2/SmartTv.java
@@ -0,0 +1,12 @@
+package Q2;
+
+public class SmartTv implements Conectavel {
+
+	@Override
+	public void conectar() {
+		// TODO Auto-generated method stub
+		System.out.println("SmartTV conectada a Internet!");
+		
+	}
+
+}
