d0c85a0e22ab27658b025a9bd7df6abacb23dd69
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-11 19:57:35 -0300

    Create Smartphone.java

---
+++ Prova2UND-1/Q2/Smartphone.java
@@ -0,0 +1,12 @@
+package Q2;
+
+public class Smartphone implements Conectavel {
+
+	@Override
+	public void conectar() {
+		// TODO Auto-generated method stub
+		System.out.println("Smartphone conectado a Internet!");
+		
+	}
+
+}
