5717985e36d4f6eb5aaca5de564e6a90f1d4c497
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-11 17:49:37 -0300

    Create Main.java

---
+++ Excecoes/Pagamento/Main.java
@@ -0,0 +1,24 @@
+package Simulado;
+
+
+import java.util.ArrayList;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		
+		PagamentoPix p1 = new PagamentoPix();
+		p1.processarPagamento(1000);
+		PagamentoCartao c1 = new PagamentoCartao();
+		c1.processarPagamento(1000);
+		
+
+		
+		
+		
+		
+	}
+
+}
