36a6eef90ac82a5c1143f4e98e5bc3be2daa18c1
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-18 20:33:48 -0300

    Update Contador.java

--- Thread/Contador.java
+++ Thread/Contador.java
@@ -1,1 +1,27 @@
+package aula1811;
+
+public class Contador extends Thread{
+	
+	protected int max;
+	protected String nome;
+	
+	public Contador(String nome, int max) {
+		this.nome = nome;
+		this.max = max;
+	}
+	
+	
+	public void contar() throws InterruptedException  {	
+		try {		
+			for(int i = 1; i <= this.max; i++) {
+				
+				Thread.sleep(1000);
+				System.out.println(this.nome + "-" + i);
+			}
+		}
+		finally {
+			System.out.println("Contagem finalizada...");
+		}
+	}
+}
 
