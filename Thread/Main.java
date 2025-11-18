c97096625079e799aa5775b27cea5964c800c6aa
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-18 20:33:32 -0300

    Update Main.java

--- Thread/Main.java
+++ Thread/Main.java
@@ -1,1 +1,38 @@
+package aula1811;
+
+public class Main  {
+	
+	public static void helloworld() {
+		System.out.println("------------------------------");
+		System.out.println("Hello World!");
+		System.out.println("Processando...");
+		System.out.println("------------------------------");
+	}
+	public static void finish() {
+		System.out.println("----------------------------------");
+		System.out.println("Processo finalizado com sucesso!");
+		System.out.println("----------------------------------");
+	}
+		
+	
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		Contador c1 = new Contador("Contador1", 5);
+		Contador c2 = new Contador("Contador2", 5);
+		
+		try {
+			
+			helloworld();
+			c1.contar();
+			c2.contar();
+		}
+		
+		finally {
+			finish();
+		}
+		
+	
+	}
+}
 
