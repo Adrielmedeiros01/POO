d386346b1faf609d9921ae823adff2d9dcf5a73b
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-18 21:34:52 -0300

    Update Main.java

--- Thread/Main.java
+++ Thread/Main.java
@@ -21,11 +21,28 @@ public class Main  {
 		Contador c1 = new Contador("Contador1", 5);
 		Contador c2 = new Contador("Contador2", 5);
 		
+		Download d1 = new Download("Download1");
+		Download d2 = new Download("Download2");
+		
 		try {
 			
 			helloworld();
-			c1.contar();
-			c2.contar();
+			//Contador normal:
+//			c1.contar();
+//			c2.contar();
+			
+			// Utilizando o Threads (Faz executar os dois processos ao mesmo tempo);
+//			c1.start();
+//			c2.start();
+	
+			// Utilizando o Runnnable (Faz executar os dois processos ao mesmo tempo):
+//			new Thread(c1).start();
+//			new Thread(c2).start();
+			
+			// Exemplo utilizando a Thread com a classe download:
+//			d1.start();
+//			d2.start();
+						
 		}
 		
 		finally {
@@ -35,4 +52,3 @@ public class Main  {
 	
 	}
 }
-
