bd13a26a96731617d963be90387dc79f74992db8
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-18 21:34:10 -0300

    Update Contador.java

--- Thread/Contador.java
+++ Thread/Contador.java
@@ -1,6 +1,7 @@
 package aula1811;
 
-public class Contador extends Thread{
+public class Contador extends Thread{	
+	// Ou implements Runnable
 	
 	protected int max;
 	protected String nome;
@@ -9,7 +10,6 @@ public class Contador extends Thread{
 		this.nome = nome;
 		this.max = max;
 	}
-	
 	
 	public void contar() throws InterruptedException  {	
 		try {		
@@ -23,5 +23,24 @@ public class Contador extends Thread{
 			System.out.println("Contagem finalizada...");
 		}
 	}
+	
+	
+	//------------------------------------------------------------------------------------------------------------------------
+	
+	
+	// Extendendo e sobrescrevendo o método run de Thread -- Também se faz o mesmo se você implementar o Runnnable:
+	@Override
+	public void run() {
+		try {
+			contar();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
 }
-
