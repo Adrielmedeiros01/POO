99c6287f5e205db9d474415644f578c2610ca1b3
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-11-25 20:57:52 -0300

    Create Main.java

---
+++ Genéricos/Main.java
@@ -0,0 +1,39 @@
+package aula2511;
+
+public class Main {
+	
+
+	public static void helloworld() {
+		System.out.println("------------------------------");
+		System.out.println("Hello World!");
+		System.out.println("Processando...");
+		System.out.println("------------------------------");
+	}
+	public static void finish() {
+		System.out.println("----------------------------------");
+		System.out.println("Processo finalizado com sucesso!");
+		System.out.println("----------------------------------");
+	}
+
+	public static void main(String[] args) {
+	
+
+		
+		try {
+			helloworld();
+			Par<String> p1 = new Par<>("Hello", "World!");
+			System.out.println(p1.getPrimeiro());
+			System.out.println(p1.getSegundo());
+			
+		}
+		
+		finally {
+			finish();
+		}
+		
+		
+		
+
+	}
+
+}
