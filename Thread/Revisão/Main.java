588787695abb22cd6c9e609f2a8293fa3344b930
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-12-02 16:05:45 -0300

    Create Main.java

---
+++ Thread/Revisão/Main.java
@@ -0,0 +1,39 @@
+package Thread;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+//		Contador c1 = new Contador();
+//		c1.start();
+		
+//		Ping p1 = new Ping();
+//		Pong p2 = new Pong();
+//		p1.start();
+//		p2.start();		
+
+//		MsgRepeat m1 = new MsgRepeat("Contagem", 10);
+//		m1.start();
+			
+//		BancoSynchronized b1 = new BancoSynchronized("Adriel");
+//		Thread t1 = new Thread(() -> b1.sacar(100));
+//      Thread t2 = new Thread(() -> b1.sacar(100));
+//		t1.start();
+//		t2.start();
+		
+		
+//		Download d1 = new Download("D1");
+//		Download d2 = new Download("D2");
+//		Download d3 = new Download("D3");		
+//		d1.start();
+//		d2.start();
+//		d3.start();
+		
+		
+		
+		
+
+	}
+
+}
