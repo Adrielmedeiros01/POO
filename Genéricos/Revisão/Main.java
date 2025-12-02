ebefbf19acd9024238e481333057d67cc338a3bf
Adriel Medeiros <130292427+Adrielmedeiros01@users.noreply.github.com>
2025-12-02 16:29:53 -0300

    Create Main.java

---
+++ Genéricos/Revisão/Main.java
@@ -0,0 +1,21 @@
+package Genericos;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+//		Caixa<Integer> c1 = new Caixa<>();
+//		c1.setValor(50);
+//		Caixa<String> c2 = new Caixa<>();
+//		c2.setValor("Java");
+//		
+//		System.out.println(c1.getValor());
+//		System.out.println(c2.getValor());
+			
+//		Par<String, Integer> p1 = new Par<>("Idade", 24);
+//		p1.exibir();
+
+	}
+
+}
